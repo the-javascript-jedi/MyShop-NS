554480ac9f73c10796ad53e29aed778ecc07bbcc
Nithin Samuel <nithin.thewebdev@gmail.com>
2021-10-14 20:29:02 +0530

    Product Model and InMemory Product Repo Built

---
+++ MyShop.Core/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        //Add a max string length of 20 to the model
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Range(0,1000)]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+        
+        //we create a constructor so that whenever we create a Product model we automatically generate an id
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }                                 
+}
---
+++ MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products;
+
+        //constructor for static initialization
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+        //we will store our current list of products into the cache.
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+        //CRUD functionality
+        //Insert
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+        //Update
+        public void Update(Product product)
+        {
+            //look in db to find if product exists
+            Product productToUpdate = products.Find(p => p.Id == product.Id);
+            if (productToUpdate != null)
+            {
+                productToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product no found");
+            }
+        }
+        //Find a single product
+        public Product Find(string Id)
+        {
+            //look in db to find if product exists
+            Product product = products.Find(p => p.Id == Id);
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+        //return a list of products that can be queried
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+        //delete a product
+        public void Delete(Product product)
+        {
+            //look in db to find if product exists
+            Product productToDelete = products.Find(p => p.Id == product.Id);
+            if (productToDelete != null)
+            {
+                products.Remove(productToDelete);
+            }
+            else
+            {
+                throw new Exception("Product not found to delete");
+            }
+        }
+    }
+}
